dc2189a5b58ea05c12db740a4742196cfc47de3f
alkaRathod70 <147063941+alkaRathod70@users.noreply.github.com>
2024-01-09 12:46:35 +0530

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -43,38 +43,6 @@ const Navbar = () => {
 <div className={nav ? 'md:hidden fixed left-0 top-0 w-full h-screen bg-black/70' : ""}>
 <div className={nav ? 'fixed left-0 top-0 w-[70%] drop-shadow-xl sm:w-[60%] md:w-[45%] h-screen bg-black/100 p-10 ease-in duration-500 ': 'fixed left-[-120%] top-0  p-10 ease-in duration-500 '}>
 
-<div className='flex w-full items-center justify-between'>
-    <pre className='flex text-xl'>
-    <FaLaptopCode size={30}/> IT Spark Labs  </pre>
-    <span className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer text-black bg-white'
-    onClick={handleNav}
-    >
-    <AiOutlineClose />
-    </span>
-    </div>
-    <div className='border-b border-gray-300 my-4 '>
-    <p className='w-[85%] md:w-[90%] py-4'>
-    Discover the World of Possibilities in IT
-    </p>
-    </div>
-    <div className='py-4 flex flex-col '>
-<ul className='uppercase'>
-    <Link href='/'>
-        <li className='py-4 text-sm '>Home</li>
-    </Link>
-    <Link href='/Projects'>
-        <li className='py-4 text-sm'>Projects</li>
-    </Link>
-    <Link href='/Skills'>
-        <li className='py-4 text-sm '>Skills</li>
-    </Link>
-    <Link href='/About'>
-        <li className='py-4 text-sm '>About</li>
-    </Link>
-    <Link href='/Contact'>
-        <li className='py-4 text-sm '>Contact</li>
-    </Link>
-        </ul>
         <div className='pt-40'>
     <p className='uppercase tracking-widest text-'> Let's Connect </p>
     <div className='flex items-center justify-between my-4 w-full sm:w-[80%]'>
