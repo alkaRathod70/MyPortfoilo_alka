95a3ac478cb7a047061039d92b020c40bb8f75d2
alkaRathod70 <147063941+alkaRathod70@users.noreply.github.com>
2024-01-10 14:19:12 +0530

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -14,13 +14,13 @@ const Navbar = () => {
     <header class="text-gray-600 body-font ">
   <div class="container mx-auto flex flex-row justify-around p-5 flex md:flex-row items-center ">
    
-       <nav class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400 flex md:flex-wrap items-center text-base justify-center"> 
-    <ul className='flex text-white flex-row justify-between'>
+       <nav class="flex justify-between md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400 flex md:flex-wrap items-center text-base justify-center"> 
+    <ul className='flex text-white flex-row '>
      <Link href='/'> 
       <li className='ml-10 text-sm uppercase hover:border-b'> Home</li>
     </Link>
      <Link href='/Projects'>
-       <li className='ml-10 px-4 text-sm uppercase hover:border-b' style={{"padding-left":"5px"}}> Projects</li>
+       <li className='ml-10 px-4 text-sm uppercase hover:border-b'> Projects</li>
     </Link>
     <Link href='/Skills'>
        <li className='ml-10 px-4 text-sm uppercase hover:border-b'> Skills</li>
