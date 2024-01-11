8c9880b5f40f365d7bc8c40c6e103904a31a2dff
alkaRathod70 <147063941+alkaRathod70@users.noreply.github.com>
2024-01-11 11:08:07 +0530

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -13,14 +13,16 @@ const Navbar = () => {
   return (
     <header class="text-gray-600 body-font ">
   <div class="container mx-auto flex flex-row justify-center p-5 flex md:flex-row items-center ">
+    
+   
+       <nav class="flex justify-center md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400 flex md:flex-wrap items-center text-base justify-center"> 
          <a class="flex title-font font-medium items-center text-gray-100 mb-4 md:mb-0">
     <pre className='flex text-xl'>
     <FaLaptopCode size={30}/> Alka Rathod </pre>
       
     </a>
-   
-       <nav class="flex justify-center md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400 flex md:flex-wrap items-center text-base justify-center"> 
-    <ul className='flex text-white flex-row gap-20 justify-center ml-5'>
+           
+           <ul className='flex text-white flex-row gap-20 justify-center ml-5'>
      <Link href='/'> 
       <li className='ml-12 text-sm uppercase hover:border-b 'style={{padding:"10px"}}> Home</li>
     </Link>
